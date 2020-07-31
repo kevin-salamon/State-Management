4de13fe7321e36650b424ddf93991af46ce9fde5
kevin-salamon <kevinwebdevs@gmail.com>
2020-07-31 12:56:23 -0400

    incorporated State data into chart

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import Header from '../reusables/Header';
 import { Bar } from 'react-chartjs-2';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPlants } from '../../actions/plantActions';
 import './chartstyle.css';
 
 class Chart extends Component {
 
     render() {
 
+        
         const barChart = 
                 <Bar
                     type = 'horizontalBar'
                     data = {{
-                        labels: ['Corn', 'Tomatoes', 'Cucumbers', 'Spinach'],
+                        labels: this.props.plants.map(plant => plant.name),
                         datasets: [{
                             label: 'Days',
                             backgroundColor: 'lightblue',
-                            data: [80, 60, 60, 30, 50]
+                            data: this.props.plants.map(plant => plant.growTime)
                         }]
     
                     }}
@@ -55,4 +59,15 @@ class Chart extends Component {
     
 }
 
-export default Chart;
+Chart.propTypes = {
+    fetchPlants: PropTypes.func.isRequired,
+    plants: PropTypes.array.isRequired,
+    newPlant: PropTypes.object
+}
+
+const mapStateToProps = state => ({
+    plants: state.plants.plants,
+    newPlant: state.plants.plant
+});
+
+export default connect(mapStateToProps, { fetchPlants })(Chart);
