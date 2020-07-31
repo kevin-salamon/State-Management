999da5618a0b72f06b06bcb20f327284193448fe
kevin-salamon <kevinwebdevs@gmail.com>
2020-07-31 12:02:46 -0400

    Updates

---
+++ precache-manifest.4f4d50e5bd67323d81ab3f80d14638b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1515498b65b8681f6560b69d060296",
+    "url": "/State-Management/index.html"
+  },
+  {
+    "revision": "61acbcc36758f30582cb",
+    "url": "/State-Management/static/css/main.eb23fc23.chunk.css"
+  },
+  {
+    "revision": "0f237bb738f5ac63642e",
+    "url": "/State-Management/static/js/2.4c99e9ec.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/State-Management/static/js/2.4c99e9ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61acbcc36758f30582cb",
+    "url": "/State-Management/static/js/main.154a65dc.chunk.js"
+  },
+  {
+    "revision": "7502407a8be155d1b9ef",
+    "url": "/State-Management/static/js/runtime-main.53d708a7.js"
+  }
+]);
