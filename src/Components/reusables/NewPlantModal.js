fe78bf5434579fc680bbd4d781574db2ef400267
kevin-salamon <kevinwebdevs@gmail.com>
2020-08-03 09:52:09 -0400

    changed modal from functional to class component for better use of redux

--- src/Components/reusables/NewPlantModal.js
+++ src/Components/reusables/NewPlantModal.js
@@ -1,132 +1,146 @@
-import React, { useRef, useState } from 'react'
+import React, { Component } from 'react'
 import Modal from 'react-bootstrap/Modal';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createPlant } from '../../actions/plantActions';
 import './modalstyle.css';
 
-const NewPlantModal = () => {
+class NewPlantModal extends Component {
     
-    const [show, setShow] = useState(false);
+    // const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    
+    constructor(){
+        super();
+        this.state = {
+            showHide : false
+        }
+    }
 
-    const idRef = useRef();
-    const nameRef = useRef();
-    const dateRef = useRef();
-    const growRef = useRef();
-    const waterRef = useRef();
-    const sunRef = useRef();
-    const pictureRef = useRef();
+    handleModalShowHide() {
+        this.setState({ showHide: !this.state.showHide })
+    }
 
-    function handleSubmit(event) {
+    // const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
+
+    // const idRef = useRef();
+    // const nameRef = useRef();
+    // const dateRef = useRef();
+    // const growRef = useRef();
+    // const waterRef = useRef();
+    // const sunRef = useRef();
+    // const pictureRef = useRef();
+
+    handleSubmit(event) {
         event.preventDefault();
         const newPlant = {
-            id: idRef.current.value,
-            name: nameRef.current.value,
-            plantDate: dateRef.current.value,
-            growTime: growRef.current.value,
-            idealWater: waterRef.current.value,
-            idealSun: sunRef.current.value,
-            picture: pictureRef.current.value,
+            id: this.getId.value,
+            name: this.getName.value,
+            plantDate: this.getPlantDate.value,
+            growTime: this.getGrowTime.value,
+            idealWater: this.getWater.value,
+            idealSun: this.getSun.value,
+            picture: this.getPicture.value
         };
 
         console.log(`Adding plant to state: ${newPlant.name}`);
         createPlant(newPlant)
         
-        handleClose();
+        this.setState({ showHide: false });
     }
 
-    return (
-        <>
-            <button className="btn header-button-add" style={{backgroundColor: "#463239", color: "white"}} onClick={handleShow}>
-                Add Plant
-            </button>
+    render() {
+        return (
+            <>
+                <button className="btn header-button-add" style={{backgroundColor: "#463239", color: "white"}} onClick={() => this.handleModalShowHide()}>
+                    Add Plant
+                </button>
 
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header style={{backgroundColor: "#7FB285", color: "white"}} closeButton>
-                    <Modal.Title style={{fontFamily: "'Roboto Slab', serif", fontSize: "30px"}}>Add Plant</Modal.Title>
-                </Modal.Header>
-                <Modal.Body style={{ backgroundColor: "#FAB3A9", borderBottomLeftRadius: "5px", borderBottomRightRadius: "5px"}}>
-                    <form>
+                <Modal show={this.state.showHide}>
+                    <Modal.Header style={{backgroundColor: "#7FB285", color: "white"}} closeButton onClick={() => this.handleModalShowHide()}>
+                        <Modal.Title style={{fontFamily: "'Roboto Slab', serif", fontSize: "30px"}}>Add Plant</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body style={{ backgroundColor: "#FAB3A9", borderBottomLeftRadius: "5px", borderBottomRightRadius: "5px"}}>
+                        <form>
 
-                        <div className="form-group text-center">
-                            <label>ID: </label><br/>
-                            <input
-                                className="input"
-                                ref={idRef}
-                                type="text"
-                                placeholder="ID"
-                            />
-                        </div>
-                        <div className="form-group text-center">
-                            <label>Name: </label><br/>
-                            <input
-                                className="input"
-                                ref={nameRef}
-                                type="text"
-                                placeholder="Name"
-                            />
-                        </div>
-                        <div className="form-group text-center">
-                            <label>Planting Date: </label><br/>
-                            <input
-                                className="input"
-                                ref={dateRef}
-                                type="text"
-                                placeholder="Planting Date"
-                            />
-                        </div>
-                        <div className="form-group text-center">
-                            <label>Days to Harvest: </label><br/>
-                            <input
-                                className="input"
-                                ref={growRef}
-                                type="text"
-                                placeholder="Days to Harvest"
-                            />
-                        </div>
-                        <div className="form-group text-center">
-                            <label>Ideal Water: </label><br/>
-                            <input
-                                className="input"
-                                ref={waterRef}
-                                type="text"
-                                placeholder="Ideal Water"
-                            />
-                        </div>
-                        <div className="form-group text-center">
-                            <label>Ideal Sun: </label><br/>
-                            <input
-                                className="input"
-                                ref={sunRef}
-                                type="text"
-                                placeholder="Ideal Sun"
-                            />
-                        </div>
-                        <div className="form-group text-center">
-                            <label>Picture Link: </label><br/>
-                            <textarea
-                                rows="3"
-                                style={{height: "65px"}}
-                                className="input"
-                                ref={pictureRef}
-                                type="text"
-                                placeholder="Full Link to Picture"
-                            />
-                        </div>
-                    </form>
+                            <div className="form-group text-center">
+                                <label>ID: </label><br/>
+                                <input
+                                    className="input"
+                                    ref={(input)=>this.getId = input}
+                                    type="text"
+                                    placeholder="ID"
+                                />
+                            </div>
+                            <div className="form-group text-center">
+                                <label>Name: </label><br/>
+                                <input
+                                    className="input"
+                                    ref={(input)=>this.getName = input}
+                                    type="text"
+                                    placeholder="Name"
+                                />
+                            </div>
+                            <div className="form-group text-center">
+                                <label>Planting Date: </label><br/>
+                                <input
+                                    className="input"
+                                    ref={(input)=>this.getPlantDate = input}
+                                    type="text"
+                                    placeholder="Planting Date"
+                                />
+                            </div>
+                            <div className="form-group text-center">
+                                <label>Days to Harvest: </label><br/>
+                                <input
+                                    className="input"
+                                    ref={(input)=>this.getGrowTime = input}
+                                    type="text"
+                                    placeholder="Days to Harvest"
+                                />
+                            </div>
+                            <div className="form-group text-center">
+                                <label>Ideal Water: </label><br/>
+                                <input
+                                    className="input"
+                                    ref={(input)=>this.getWater = input}
+                                    type="text"
+                                    placeholder="Ideal Water"
+                                />
+                            </div>
+                            <div className="form-group text-center">
+                                <label>Ideal Sun: </label><br/>
+                                <input
+                                    className="input"
+                                    ref={(input)=>this.getSun = input}
+                                    type="text"
+                                    placeholder="Ideal Sun"
+                                />
+                            </div>
+                            <div className="form-group text-center">
+                                <label>Picture Link: </label><br/>
+                                <textarea
+                                    rows="3"
+                                    style={{height: "65px"}}
+                                    className="input"
+                                    ref={(input)=>this.getPicture = input}
+                                    type="text"
+                                    placeholder="Full Link to Picture"
+                                />
+                            </div>
+                        </form>
 
-                    <button className="btn btn-light header-button" style={{margin: "0 auto"}} onClick={handleSubmit}>
-                        Submit
-                    </button>
+                        <button className="btn btn-light header-button" style={{margin: "0 auto"}} onClick={this.handleSubmit}>
+                            Submit
+                        </button>
 
-                </Modal.Body>
+                    </Modal.Body>
 
-            </Modal>
-        </>
-    );
+                </Modal>
+            </>
+        );
+    }
 }
 
 NewPlantModal.propTypes = {
