918f65ec37617a2378ee60acce604a1b499137a7
kevin-salamon <kevinwebdevs@gmail.com>
2020-08-05 18:43:23 -0400

    Updates

--- precache-manifest.ff6696e346095af8c980f4f418277f76.js
+++ precache-manifest.ff6696e346095af8c980f4f418277f76.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e85e072c688c53302280adf141b8418b",
+    "revision": "627aa63dc748199bfd33ccaaf78f68ed",
     "url": "/State-Management/index.html"
   },
   {
-    "revision": "2a45b8100b356d478c0e",
+    "revision": "da2008b3b956ecb44c89",
     "url": "/State-Management/static/css/main.747641c1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/State-Management/static/js/2.ebc0ff61.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2a45b8100b356d478c0e",
-    "url": "/State-Management/static/js/main.ec9c2245.chunk.js"
+    "revision": "da2008b3b956ecb44c89",
+    "url": "/State-Management/static/js/main.f8e4fde2.chunk.js"
   },
   {
     "revision": "7502407a8be155d1b9ef",
